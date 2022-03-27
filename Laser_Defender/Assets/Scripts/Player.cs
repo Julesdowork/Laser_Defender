86a94b4f971baa15e3a01f059d87d62ab97aeccd
Julian McNeill <julesdowork@gmail.com>
2022-03-27 18:16:55 -0400

    Boundaries

--- Laser_Defender/Assets/Scripts/Player.cs
+++ Laser_Defender/Assets/Scripts/Player.cs
@@ -7,6 +7,17 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
     Vector2 rawInput;
+    Vector2 minBounds;
+    Vector2 maxBounds;
+    [SerializeField] float paddingLeft;
+    [SerializeField] float paddingRight;
+    [SerializeField] float paddingTop;
+    [SerializeField] float paddingBottom;
+
+    void Start()
+    {
+        InitBounds();
+    }
 
     void Update()
     {
@@ -19,9 +30,19 @@ public class Player : MonoBehaviour
         Debug.Log(rawInput);
     }
 
+    private void InitBounds()
+    {
+        Camera mainCam = Camera.main;
+        minBounds = mainCam.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCam.ViewportToWorldPoint(new Vector3(1, 1));
+    }
+
     private void Move()
     {
-        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPos;
     }
 }
