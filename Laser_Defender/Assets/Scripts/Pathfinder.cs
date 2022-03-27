27ae64a03f8a0b65d853f23d32127ddbb6576afc
Julian McNeill <julesdowork@gmail.com>
2022-03-27 18:37:10 -0400

    Pathfinding

---
+++ Laser_Defender/Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    [SerializeField] WaveConfigSO waveConfig;
+    List<Transform> waypoints;
+    int waypointIndex = 0;
+
+    void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    void Update()
+    {
+        FollowPath();
+    }
+
+    private void FollowPath()
+    {
+        if (waypointIndex < waypoints.Count)
+        {
+            Vector3 targetPos = waypoints[waypointIndex].position;
+            float delta = waveConfig.GetMoveSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, delta);
+            if (transform.position == targetPos)
+                waypointIndex++;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
